3858d6667f5e22f816a74c7b04c3a53900d7c1ca
kiokeda <289049473+kioskedamed@users.noreply.github.com>
2026-06-30 16:56:59 -0500

    Add greeting function with alert message
    Se creo la alerta para crear un contador de cargas de la página web y ademas una laerta que indica el creador del contenido.

---
+++ Js.js
@@ -0,0 +1,10 @@
+//contador de cargas de la página.
+function salu(){
+  let num, num2, num3, contt = 0 = Number;
+  const num1 = 1 = Number;
+  num = num + num1;
+  //alerta de uso del creador de contenido
+  alert("Página web creada por José fernando cano peláez de la empresa:\nDilmed agilizando tu vida.");
+}
+
+
