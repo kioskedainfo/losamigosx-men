0fba6038c73f65efca05838494920c00629b8ec6
kiokeda <289049473+kioskedamed@users.noreply.github.com>
2026-06-30 17:00:51 -0500

    Modify alert message with donation details
    Updated alert message to include donation information.

--- Js.js
+++ Js.js
@@ -4,7 +4,7 @@ function salu(){
   const num1 = 1 = Number;
   num = num + num1;
   //alerta de uso del creador de contenido
-  alert("Página web creada por José fernando cano peláez de la empresa:\nDilmed agilizando tu vida.");
+  alert("Página web creada por José fernando cano peláez de la empresa:\nDilmed agilizando tu vida.\nSi deseas hacer una donación o dar una propina puedes hacerla en la cuenta de ahorros bancolombia No° 47475277321\nCon el nombre de José fernando cano peláez.");
 }
 
 
